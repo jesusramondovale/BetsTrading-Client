e9623b96e9d735d83b8ab598817b6671b0e28695
jesus.dovale <jesus.dovale@mecalux.com>
2023-12-10 15:33:07 +0100

    Comunicación con API Auth via HTTPS (self-signed)

--- lib/AuthService.dart
+++ lib/AuthService.dart
@@ -4,40 +4,39 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class AuthService {
-  static const String apiUrl = 'http://192.168.1.37:5000/api';
+  static const String apiUrl = 'https://192.168.1.38:44346/api/Auth/LogIn';
 
   Future<Map<String, dynamic>> logIn(String username, String password) async {
     try {
-      final response = await http.post(
-        
-        Uri.parse('$apiUrl/Auth/LogIn'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'username': username, 'password': password}),
-        
-      ).timeout(const Duration(seconds: 3));
+
+      final Map<String, dynamic> data = {'username': username, 'password': password};
+      bool _certificateCheck(X509Certificate cert, String host, int port) => true;
+      HttpClient client = new HttpClient()..badCertificateCallback = (_certificateCheck);
+
+      final HttpClientRequest request = await client.postUrl(Uri.parse(apiUrl));
+      request.headers.set('Content-Type', 'application/json');
+      request.write(jsonEncode(data));
+
+      final HttpClientResponse response = await request.close();
 
       if (response.statusCode == 200) {
-        final Map<String, dynamic> decodedBody = jsonDecode(response.body);
+        final Map<String, dynamic> decodedBody = jsonDecode(await response.transform(utf8.decoder).join());
         final String message = decodedBody['message'];
         return {'success': true, 'message': message};
-      }
-
-      else {
-        final Map<String, dynamic> decodedBody = jsonDecode(response.body);
+      } else {
+        final Map<String, dynamic> decodedBody = jsonDecode(await response.transform(utf8.decoder).join());
         final String message = decodedBody['message'];
         return {'success': false, 'message': message};
       }
-
     } catch (e) {
-
-      var theFuckingType = e.runtimeType;
-      if (e.runtimeType == TimeoutException){
-          return {'success': false, 'message': "Server not responding. Try again later"};
-        }
-        if (e.runtimeType.toString() == "_ClientSocketException"){
-          return {'success': false, 'message': "Can't connect. Check your internet connection"};
-        }
-        return {'success': false, 'message': '$e'};
+      if (e.runtimeType == TimeoutException) {
+        return {'success': false, 'message': "Server not responding. Try again later"};
+      }
+      if (e.runtimeType.toString() == "_ClientSocketException") {
+        return {'success': false, 'message': "Can't connect. Check your internet connection"};
+      }
+      return {'success': false, 'message': '$e'};
     }
   }
+
 }
