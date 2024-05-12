249d0bb2d108152d7a917fc077c51361695c6437
jesus.dovale <jesus.dovale@mecalux.com>
2024-05-12 18:44:06 +0200

    Markets view: Full Icons + Style

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 
-String CODE_VERSION = '24.132';
+String CODE_VERSION = '24.133';
 
 Future<void> main() async {
 
--- lib/ui/financial_assets_view.dart
+++ lib/ui/financial_assets_view.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:client_0_0_1/services/AssetsService.dart';
 import 'package:flutter/material.dart';
 
@@ -22,37 +24,40 @@ class FinancialAssetsViewState extends State<FinancialAssetsView> {
   @override
   void initState() {
     super.initState();
-    selectedGroup = 'ETFs';
-
-    _loadAssets(groupId);
+    _loadAssets(groupId); // Load assets initially with default groupId
   }
 
   Future<void> _initGroups() async {
     final strings = LocalizedStrings.of(_context);
     groups = [
+      strings?.indexes ?? 'Indexes', // Assuming these are non-nullable fields
       'ETFs',
-      strings?.indexes ?? 'Indexes',
       'Cryptos',
       strings?.shares ?? 'Shares',
       strings?.commodities ?? 'Commodities',
     ];
+    if (selectedGroup == null && groups.isNotEmpty) {
+      selectedGroup = groups.first;
+    }
   }
 
   void _loadAssets(int id) {
-    String theGroup = '';
-    switch (id){
-      case 0: theGroup = 'ETFs';
-      case 1: theGroup = 'Indexes';
-      case 2: theGroup = 'Cryptos';
-      case 3: theGroup = 'Shares';
-      case 4: theGroup = 'Commodities';
+    String? theGroup;
+    switch (id) {
+      case 0: theGroup = 'Indexes'; break;
+      case 1: theGroup = 'ETFs'; break;
+      case 2: theGroup = 'Cryptos'; break;
+      case 3: theGroup = 'Shares'; break;
+      case 4: theGroup = 'Commodities'; break;
     }
 
-    AssetsService().getFinancialAssetsByGroup(theGroup).then((assets) {
-      setState(() {
-        this.assets = assets!;
+    if (theGroup != null) {
+      AssetsService().getFinancialAssetsByGroup(theGroup).then((newAssets) {
+        setState(() {
+          assets = newAssets ?? [];
+        });
       });
-    });
+    }
   }
 
   @override
@@ -63,7 +68,7 @@ class FinancialAssetsViewState extends State<FinancialAssetsView> {
       future: _initGroups(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
-          return _buildContent(groupId);
+          return _buildContent();
         } else {
           return const Center(child: CircularProgressIndicator());
         }
@@ -71,31 +76,62 @@ class FinancialAssetsViewState extends State<FinancialAssetsView> {
     );
   }
 
-  Widget _buildContent(groupId) {
+  void _onGroupChanged(String? newValue) {
+    if (newValue == null) return;
+
+    setState(() {
+      selectedGroup = newValue;
+      groupId = groups.indexOf(newValue);
+      debugPrint('Selected group ID: $groupId');
+      _loadAssets(groupId);
+    });
+  }
+
+  List<DropdownMenuItem<String>> _buildDropdownMenuItems() {
+    return groups.map((String value) {
+      return DropdownMenuItem<String>(
+        value: value,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(value),
+        ),
+      );
+    }).toList();
+  }
+
+  Widget _buildContent() {
     return Column(
       children: [
         DropdownButton<String>(
           value: selectedGroup,
-          onChanged: (value) {
-            setState(() {
-              selectedGroup = value!;
-              groupId = groups.indexOf(selectedGroup!);
-              print("$groupId");
-              _loadAssets(groupId!);
-            });
-          },
-          items: groups.map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
+          onChanged: _onGroupChanged,
+          items: _buildDropdownMenuItems(),
+          style: const TextStyle(
+            color: Colors.deepPurpleAccent,
+            fontSize: 28.0,
+            fontWeight: FontWeight.normal,
+          ),
+          icon: const Icon(Icons.arrow_drop_down, color: Colors.deepPurpleAccent, size: 45),
+          underline: Container(
+            height: 1,
+            color: Colors.deepPurpleAccent
+          ),
+          dropdownColor:Theme.of(context).brightness == Brightness.dark
+              ? Colors.black
+              : Colors.white,
+          elevation: 16,
+          borderRadius: BorderRadius.circular(25),
+          isExpanded: true,
+          iconEnabledColor: Colors.deepPurple,
+          iconDisabledColor: Colors.grey,
+          itemHeight: 60,
         ),
+
         Expanded(
           child: ListView.builder(
             itemCount: assets.length,
             itemBuilder: (context, index) {
-              FinancialAsset asset = assets[index];
+              final FinancialAsset asset = assets[index];
               return GestureDetector(
                 onTap: () {
                   Navigator.push(
@@ -109,21 +145,10 @@ class FinancialAssetsViewState extends State<FinancialAssetsView> {
                 },
                 child: ListTile(
                   leading: asset.icon.isNotEmpty
-                      ? asset.imageFromBase64String()
+                      ? Image.memory(base64Decode(asset.icon))
                       : null,
-                  title: Text(
-                    asset.name,
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                  subtitle: Text(
-                    '${asset.group} - ${asset.country}',
-                    style: const TextStyle(
-                      fontSize: 14,
-                    ),
-                  ),
-                  contentPadding: const EdgeInsets.only(left: 16),
+                  title: Text(asset.name),
+                  subtitle: Text('${asset.group} - ${asset.country}'),
                 ),
               );
             },
