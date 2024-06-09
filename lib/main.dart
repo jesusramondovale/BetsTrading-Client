680c4e883ed4e3faf9ebab36ccd7e6b79b000a73
jesus.dovale <jesus.dovale@mecalux.com>
2024-06-09 14:26:00 +0200

    Update Code Version

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 
-String CODE_VERSION = '24.160.1';
+String CODE_VERSION = '24.161.1';
 
 Future<void> main() async {
 
