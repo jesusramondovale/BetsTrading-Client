fb96e55497bb29eaef8b30c02eea6b4de13033c4
jesus.dovale <jesus.dovale@mecalux.com>
2024-09-08 15:06:09 +0200

    Added tag

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -2,7 +2,7 @@ class Config {
 
   static const PUBLIC_DOMAIN = '90.175.130.133';
   static const SERVICE_PORT = '44346';
-  static const CODE_VERSION = '24.251.2';
+  static const CODE_VERSION = 'TEST_NOTIFICATIONS';
   static const IP_GEOLOCALIZER_TOKEN = 'd99daa2befa6f8';
 
   // DEV
