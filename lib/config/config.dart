1336a13dac552f14efc2d3ef1cca5c54032d1094
jesus.dovale <jesus.dovale@mecalux.com>
2025-01-02 14:31:37 +0100

    Updated endpoint IP

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,6 +1,6 @@
 class Config {
 
-  static const PUBLIC_DOMAIN = '90.175.130.133';
+  static const PUBLIC_DOMAIN = '90.175.130.30';
   static const SERVICE_PORT = '44346';
   static const CODE_VERSION = '24.357.1';
   static const ADMOB_APP_ID = 'ca-app-pub-2465898294053562/1629478765';
