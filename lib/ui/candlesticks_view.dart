074348f9ff2e51ad91d565408cb2718dc857c5fe
Jesús Ramón <jesus.fabero@gmail.com>
2025-10-17 14:07:02 +0200

    Fix candlesticks chart zoom on high timeframes

--- lib/ui/candlesticks_view.dart
+++ lib/ui/candlesticks_view.dart
@@ -102,7 +102,7 @@ class CandlesticksViewState extends State<CandlesticksView> {
                               chartTitle: widget.name,
                               ticker: widget.ticker,
                               iconPath: widget.iconPath,
-                              extraHours: Common().hoursUntilLatestEndDate(_zonesNotifier.value, _candles.first.date),
+                              extraHours: (Common().hoursUntilLatestEndDate(_zonesNotifier.value, _candles.first.date)/TimeframeManager.current.value).ceil()
                             ),
                         Positioned(
                           top: 10.0,
