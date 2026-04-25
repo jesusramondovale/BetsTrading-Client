c98536cbb3cc21e25030ab5b20333640e6b4fe9c
Jesús Ramón <jesus.fabero@gmail.com>
2026-04-25 14:37:24 +0200

    Fix missing import

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ import 'package:betrader/services/bets_service.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_stripe/flutter_stripe.dart' as stripe;
 import 'dart:async';
 
